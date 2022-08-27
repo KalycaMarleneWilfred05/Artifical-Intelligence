f0a4869575922fe2d586c672dda64215ca2df494
KalycaMarleneWilfred05 <110885488+KalycaMarleneWilfred05@users.noreply.github.com>
2022-08-27 13:56:15 +0530

    Add files via upload

---
+++ Positive Nos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+start, end = -4, 19
+ 
+for num in range(start, end + 1):
+     
+    if num >= 0:
+        print(num, end = " ")
+
+
+# In[ ]:
+
+
+
+
